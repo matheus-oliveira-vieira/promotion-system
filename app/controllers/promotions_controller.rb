38358eced4174589a48ad8f2a581e1984991016e
Matheus Oliveira <matheus.oliveirav@gmail.com>
2021-02-02 08:47:02 -0300

    Adicionando cupons

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -23,4 +23,15 @@ class PromotionsController < ApplicationController
       render "new"
     end
   end
+
+  def generate_coupons
+    @promotion = Promotion.find(params[:id])
+    (1..@promotion.coupon_quantity).each do |number|
+      Coupon.create!(code: "#{@promotion.code}-#{'%04d' % number}", promotion: @promotion)
+      
+    end
+    flash[:notice] = 'Cupons gerados com sucesso'
+    redirect_to @promotion
+  end
+
 end
--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,4 +1,5 @@
 class Promotion < ApplicationRecord
+  has_many :coupons
   validates :name, :code, :discount_rate, :coupon_quantity, :expiration_date, presence: true
   validates :code, uniqueness: true
 end
